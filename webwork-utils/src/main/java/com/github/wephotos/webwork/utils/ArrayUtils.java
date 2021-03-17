a42ebe01c902cf2fa2f1680ec225f40b8fbd1b4e
TQ <tq_email@qq.com>
2021-03-17 20:48:39 +0800

    数组工具类、加密工具类

---
+++ webwork-utils/src/main/java/com/github/wephotos/webwork/utils/ArrayUtils.java
@@ -0,0 +1,10 @@
+package com.github.wephotos.webwork.utils;
+
+/**
+ * 数组工具类
+ * @author TQ
+ *
+ */
+public class ArrayUtils extends org.apache.commons.lang3.ArrayUtils {
+
+}
---
+++ webwork-utils/src/main/java/com/github/wephotos/webwork/utils/DigestUtils.java
@@ -0,0 +1,20 @@
+package com.github.wephotos.webwork.utils;
+
+/**
+ * 加密工具类
+ * @author TQ
+ *
+ */
+public class DigestUtils extends org.apache.commons.codec.digest.DigestUtils {
+
+	/**
+	 * Preserves binary compatibility only. As for previous versions does not
+	 * provide useful behavior
+	 * 
+	 * @deprecated since 1.11; only useful to preserve binary compatibility
+	 */
+	@Deprecated
+	public DigestUtils() {
+		super();
+	}
+}
