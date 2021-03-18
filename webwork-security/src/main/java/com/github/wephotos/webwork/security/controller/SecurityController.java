29d4d2f61c856aa8785bab7a14d5d6b766460749
TQ <tq_email@qq.com>
2021-03-18 22:44:22 +0800

    添加依赖

--- webwork-security/src/main/java/com/github/wephotos/webwork/security/controller/SecurityController.java
+++ webwork-security/src/main/java/com/github/wephotos/webwork/security/controller/SecurityController.java
@@ -1,17 +1,18 @@
 package com.github.wephotos.webwork.security.controller;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.github.wephotos.webwork.http.RestObject;
 import com.github.wephotos.webwork.security.entity.User;
 import com.github.wephotos.webwork.security.entity.UserAuth;
 import com.github.wephotos.webwork.security.service.SecurityService;
 import com.github.wephotos.webwork.security.storage.SessionUserStorage;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
 
 /**
  * API接口
@@ -20,12 +21,12 @@ import javax.servlet.http.HttpSession;
  */
 @RestController
 @RequestMapping("/security")
-@ConditionalOnBean(value = SecurityService.class)
 public class SecurityController {
 
     /**
      * 认证服务
      */
+	@Lazy
     @Resource
     private SecurityService securityService;
 
