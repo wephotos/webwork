37f2ef4f6adebed66bd2fb8909698f8e61bad7ba
TianQi <tq_email@qq.com>
2021-04-30 21:41:07 +0800

    优化WEB环境判断逻辑

--- webwork-logging/src/main/java/com/github/wephotos/webwork/logging/LoggerUtils.java
+++ webwork-logging/src/main/java/com/github/wephotos/webwork/logging/LoggerUtils.java
@@ -16,23 +16,21 @@ public final class LoggerUtils {
 	 */
 	public static final String WEB_CLASS_NAME = "javax.servlet.Servlet";
 	// 是否web环境
-	private static boolean WEB_ENV = false;
-	// 是否执行过WEB环境判断逻辑
-	private static AtomicBoolean ATOMIC_WEB_ENV = new AtomicBoolean(false);
+	private static volatile Boolean WEB_ENV;
 	
 	/**
 	 * 判断当前环境是否是WEB环境
 	 * @return
 	 */
 	public static boolean isWebEnv() {
-		if(!ATOMIC_WEB_ENV.compareAndSet(false, true)) {
+		if(WEB_ENV != null) {
 			return WEB_ENV;
 		}
 		try {
 			return (WEB_ENV = null != Class.forName(WEB_CLASS_NAME));
 		} catch (ClassNotFoundException e) {
 			Util.report(e.getMessage());
-			return false;
+			return (WEB_ENV = false);
 		}
 	}
 }
